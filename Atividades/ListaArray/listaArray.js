c7d6ca165a6b14f92beb2549ca5cdd0cd913b881
pedrolaelbzr <pedrolael@outlook.com>
2023-08-14 13:26:36 -0300

    Atualização atividade listaArray

--- Atividades/ListaArray/listaArray.js
+++ Atividades/ListaArray/listaArray.js
@@ -8,3 +8,44 @@ coresFavoritas.splice(1 ,1 , 'Marrom')
 idades.splice(3, 1, '18')
 
 console.log(nomes, idades, coresFavoritas)
+
+/*
+
+SAÍDA:
+
+[
+  'Pedro',   'Ana',
+  'Thiago',  'Clara',
+  'João',    'Letícia',
+  'Gabriel', 'Paloma',
+  'Lucas',   'Lana'
+] [
+  '16', '24', '15',
+  '19', '20', '32',
+  '12', '27', '8',
+  '10'
+] [
+  'Azul',    'Roxo',
+  'Laranja', 'Violeta',
+  'Branco',  'Lilás',
+  'Preto',   'Verde',
+  'Amarelo', 'Dourado'
+]
+[
+  'Pedro',   'Ana',
+  'Thiago',  'Clara',
+  'João',    'Letícia',
+  'Gabriel', 'Paloma',
+  'Lucas',   'Lana'
+] [
+  '16', '24', '15',
+  '18', '20', '32',
+  '12', '27', '8',
+  '10'
+] [
+  'Azul',    'Marrom',
+  'Laranja', 'Violeta',
+  'Branco',  'Lilás',
+  'Preto',   'Verde',
+  'Amarelo', 'Dourado'
+  */
