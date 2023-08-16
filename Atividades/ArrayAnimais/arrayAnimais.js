73341a97e0cf0c9b8edce432c52932a5bbce42bb
pedrolaelbzr <pedrolael@outlook.com>
2023-08-16 18:05:45 -0300

    Atividade matriz animais

---
+++ Atividades/ArrayAnimais/arrayAnimais.js
@@ -0,0 +1,9 @@
+let animais = [
+    {Nome:'Cachorro', Espécie:'Yorkshire',Idade:10},
+    {Nome:'Gato', Espécie:'Siamês', Idade:1},
+    {Nome:'Pássaro', Espécie:'Calopsita', Idade:3}
+]
+
+console.log('Nome do primeiro animal: ', animais[0].Nome) //Saída: Cachorro
+console.log('Espécie do segundo animal: ', animais[1].Espécie)//Saída: Siamês
+console.log('Idade do terceiro animal: ', animais[2].Idade)//Saída: 3
