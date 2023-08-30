e331bd04907cfe9e310133d6b9e714bda46641fc
Pedro Lael Bezerra Alencar Silva <pedrolael@outlook.com>
2023-08-29 23:33:55 -0300

    Calculadora completa

--- HTML/Calculadora/Index.js
+++ HTML/Calculadora/Index.js
@@ -1,10 +1,25 @@
 let displayValue = ""
-let opNegativo = ""
+let opNegativo = document.getElementById("changeop")
+
 function appendToDisplay(value){
     displayValue += value
-    document.getElementById("display").innerHTML = displayValue
+    document.getElementById("display").innerHTML = (displayValue)
+    
 
 }
+
+
+function porcNum (value){
+displayValue += value
+document.getElementById("Display").innerHTML = displayValue
+    if (porcNum = true){
+        clearDisplay
+    
+
+    }
+
+}
+
 
 function clearDisplay(){
     displayValue = ''
@@ -12,8 +27,9 @@ function clearDisplay(){
 
 }
 
-function changeOp(){
-    opNegativo = `-${displayV}`
+function changeOp() {
+    if(changeOp)
+    opNegativo = `-${displayValue}`
     document.getElementById("display").innerHTML = opNegativo
 
 }
